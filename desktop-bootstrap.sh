8347558cac6a2f00fdb63a5a3fa4029d2bc29753
Ricardo Borges <ricardo93borges@gmail.com>
2020-07-11 19:57:21 -0300

    Update desktop-bootstrap.sh

--- desktop-bootstrap.sh
+++ desktop-bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gnome-terminal --geometry 170x25+1920+890 --working-directory=/home/ricardo/workspace
-/usr/bin/python3.8 bootstrap-desktop.py --file config-sample.txt
+gnome-terminal --geometry 170x25+0+0 --working-directory=/home
+/usr/bin/python3.8 desktop-bootstrap.py --file config-sample.txt
